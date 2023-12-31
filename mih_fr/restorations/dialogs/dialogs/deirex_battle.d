7a613bd14d0ae062683a282a3995507d09344028
Angel <angel@pearlgates.net>
2023-12-31 09:41:10 +0100

    Framework for new version

--- mih_fr/restorations/dialogs/dialogs/deirex_battle.d
+++
@@ -1,7 +0,0 @@
-ADD_STATE_TRIGGER "jarlich" 1
-  ~NumTimesTalkedTo(0)~
-
-REPLACE_STATE_TRIGGER "jarlich" 2
-  ~PartyHasItem("miscbe") NumTimesTalkedToGT(0)~
-
-
--- mih_fr/restorations/dialogs/dialogs/volos_travels.d
+++
@@ -1,7 +0,0 @@
-ADD_STATE_TRIGGER "%tutu_var%volo" 0
-  ~AreaCheck("%Nashkel_BelchingDragonTavern%")~
-
-ADD_STATE_TRIGGER "%tutu_var%volo" 1
-  ~AreaCheck("%Nashkel_BelchingDragonTavern%")~
-
-
