2ac71d57c49bc2b08ef6d6bf05b9703a631cd738
Angel <angel@pearlgates.net>
2023-12-31 09:07:20 +0100

    Saving all open work.

--- mih_fr/restorations/dialogs/dialogs/bg1_minor_dialogs.d
+++ mih_fr/restorations/dialogs/dialogs/bg1_minor_dialogs.d
@@ -9,4 +9,18 @@ ADD_STATE_TRIGGER "%tutu_var%bently" 3
 SET_WEIGHT "%tutu_var%bently" 3 #1
 
 
+// Restore a minor line to an iron throne guard.
+
+ADD_TRANS_ACTION "iron11"
+BEGIN 6 END	// State(s)
+BEGIN 0 END	// Transaction(s)
+  ~Enemy()~
+
+ALTER_TRANS "iron11"
+BEGIN 1 END	// State(s)
+BEGIN 4 END	// Transaction(s)
+BEGIN
+  "EPILOGUE"	~GOTO 6~
+END
+
 
