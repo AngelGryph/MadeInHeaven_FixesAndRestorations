300c7e72594541a1b129454da3d1c998c0f18fb7
Angel <angel@pearlgates.net>
2024-01-17 10:56:08 +0100

    Reaction trigger target correction

--- mih_fr/restorations/volos_travels/resources/volos_travels.d
+++
@@ -1,8 +0,0 @@
-ADD_STATE_TRIGGER "volo" 0
-  ~AreaCheck("%Nashkel_BelchingDragonTavern%")~
-
-ADD_STATE_TRIGGER "volo" 1
-  ~AreaCheck("%Nashkel_BelchingDragonTavern%")~
-
-ADD_STATE_TRIGGER "volose" 6
-  ~ReactionLT(LastTalkedToBy(Myself), NEUTRAL_LOWER)~
